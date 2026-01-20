908bad4188a7e1655bcc98ddf93c748089f64053
Douglas de Barros <douglasbl18@gmail.com>
2026-01-20 16:55:15 -0300

    Navegação: tela de login e formulário de login.

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,17 +1,34 @@
-import { useHistory } from "react-router-dom";
+import { useState } from "react";
 
 export const Login = () => {
-    const history = useHistory();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const handleClick = () => {
-        history.push('/pagina-inicial')
+    const handleEntrar = () => {
+        console.log(email)
+        console.log(password)
+
     }
 
     return (
         <div>
-            Login
+            <form>
 
-            <button onClick={handleClick}>Página inicial</button>
+                <label>
+                    <span>Email</span>
+                    <input value={email} onChange={e => setEmail(e.target.value)} />
+                </label>
+
+                <label>
+                    <span>Senha</span>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                </label>
+
+                <button type="button" onClick={handleEntrar}>
+                    Entrar
+                </button>
+
+            </form>
         </div>
     );
 }
