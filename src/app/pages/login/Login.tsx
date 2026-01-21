b782457ffd96047934f2ac1959a0d31725ed9566
Douglas de Barros <douglasbl18@gmail.com>
2026-01-21 11:56:06 -0300

    Função: introdução useMemo.

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,8 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 export const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const emailLength = useMemo(() => {
+        console.log ('Executou');
+        return email.length * 1000;
+    }, [email.length]);
 
     useEffect(() => {
         if (window.confirm('Você é homem?')) {
@@ -29,6 +34,7 @@ export const Login = () => {
     return (
         <div>
             <form>
+                <p>Quantidade de caracteres no email: {emailLength}</p>
 
                 <label>
                     <span>Email</span>
