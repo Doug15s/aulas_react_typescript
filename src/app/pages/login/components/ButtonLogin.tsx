aca9264fe10fb89576aa28e89aa4403b9e910975
Douglas de Barros <douglasbl18@gmail.com>
2026-01-28 11:10:22 -0300

    Função: adicionando react hook customizado.

--- src/app/pages/login/components/ButtonLogin.tsx
+++ src/app/pages/login/components/ButtonLogin.tsx
@@ -7,6 +7,7 @@ interface IButtonLoginProps {
 }
 
 export const ButtonLogin: React.FC<IButtonLoginProps> = ({ type, onClick, children }) => {
+
     return (
         <button type={type} onClick={onClick}>
             {children}
